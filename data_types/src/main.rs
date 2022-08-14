5f8da9ef2c6cb2aa925eb081e0ea9d79f1fb8018
Nil Ventosa <nil.ventosa@sword-group.com>
2022-08-14 07:35:46 +0200

    Continue

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -19,4 +19,18 @@ fn main() {
     let element = a[index];
 
     println!("The value of the element at index {index} is: {element}");
+
+
+    // Tupple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x,y,z) = tup;
+    println!("The value of y is {y}");
+
+    // Array
+    let _a = [1,2,3,4];
+    let _b: [i32; 4] = [1,1,1,1];
+    let c = [3;5];
+    println!("The fouth element of c is {}", c[3]);
+
+
 }
