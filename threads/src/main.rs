a9bb9f1a98ec44aff6eda5dcd1ed4e15d39254c1
nilventosa <ventosa@gmail.com>
2022-10-01 15:08:07 +0200

    More from threads

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,4 +1,5 @@
 use std::sync::mpsc;
+use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration; // Multiple Producer Single Consumer
 
@@ -8,15 +9,41 @@ fn main() {
 }
 
 fn channels() {
+
     let (tx, rx) = mpsc::channel();
+    let tx1 = Sender::clone(&tx);
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
 
 fn threads() {
